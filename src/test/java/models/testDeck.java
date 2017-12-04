375b1b61d92ab4bb5adb6171f231b27d16cb9cc5
Sean Cramsey <cramseys@oregonstate.edu>
2017-12-03 21:54:35 -0800

    Add deck tests

--- src/test/java/models/testDeck.java
+++ src/test/java/models/testDeck.java
@@ -9,7 +9,28 @@ public class testDeck
   @Test
   public void testDeck()
   {
-  //NOT SURE WHAT TO TEST
+    Deck deck = new Deck();
+    assertNotNull(deck);
+    assertEquals(52, deck.size());
+    for(int i = 0; i < 52; i++){
+      assertNotNull(deck.cards[i]);
+    }
+    /*******************************/
+    for(int i = 0; i < 4; i++){
+      for(int j = 2; j < 15; i++){
+        assertEquals(j, deck.cards[i].value); //right?
+      }
+    }
+    /******************************/
   }
+  @testing
+  public void testShuffle(){
+    Deck deck = new Deck();
+    Deck deckB = new Deck();
 
+    deck.shuffle();
+    for (int i = 0; i < 52; i++){
+      assertFalse(deck.cards[i], deck.cards[i]); //see that cards changed position, dont know if this will work though
+    }
+  }
 }
